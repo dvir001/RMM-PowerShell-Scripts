6a7f5c5c47ff3398e041a93a55c5b262b0c5a1bb
Dvir <39403717+dvir001@users.noreply.github.com>
2021-05-20 21:02:36 +0300

    Create Install-VLC.ps1

---
+++ Programs/Install-VLC.ps1
@@ -0,0 +1,5 @@
+if(test-path "C:\ProgramData\chocolatey\choco.exe") { }
+else { Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) }
+# Install apps:
+C:\ProgramData\chocolatey\choco.exe install vlc -y --acceptlicense
+
