cc1a80252f2f70045e2af20937f441d9ec0b3c1c
Dvir <39403717+dvir001@users.noreply.github.com>
2021-05-31 02:24:53 +0300

    Update Set-Language.ps1

--- Language/Set-Language.ps1
+++ Language/Set-Language.ps1
@@ -1,1 +1,33 @@
+function Set-Language-EN_HE
+{
+	$source = 'https://www.forensit.com/Downloads/Profwiz.msi'
+	$dir = 'C:\Windows\Temp'
+	$name = 'Win10_LP_En.cab'
+	$systemLocale = 'en-US'
+	$language = 'en-US'
+	$language2 = 'he'
+	
+	Dism /online /Add-Package /PackagePath:$dir\$name
+	Set-WinSystemLocale $systemLocale
+	
+	$UserLanguageList = New-WinUserLanguageList -Language $language
+	$UserLanguageList.Add("$language2")
+	Set-WinUserLanguageList -LanguageList $UserLanguageList
+}
 
+function Set-Language-HE_EN
+{
+	$source = 'https://www.forensit.com/Downloads/Profwiz.msi'
+	$dir = 'C:\Windows\Temp'
+	$name = 'Win10_LP_He.cab'
+	$systemLocale = 'he-IL'
+	$language = 'he'
+	$language2 = 'en-US'
+	
+	Dism /online /Add-Package /PackagePath:$dir\$name
+	Set-WinSystemLocale $systemLocale
+	
+	$UserLanguageList = New-WinUserLanguageList -Language $language
+	$UserLanguageList.Add("$language2")
+	Set-WinUserLanguageList -LanguageList $UserLanguageList
+}
