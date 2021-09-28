456cd020fcf3eee08f3c800889b993fa8bbcc8da
Dvir <39403717+dvir001@users.noreply.github.com>
2021-09-28 20:50:06 +0300

    Update Install-Drivers.ps1

--- Programs/Install-Drivers.ps1
+++ Programs/Install-Drivers.ps1
@@ -1,46 +1,246 @@
-$PCInfo = Get-WMIObject -Query "Select * from Win32_ComputerSystem" | Select-Object -Property Manufacturer, Model
-
-# Locate Dell PC Manufacturer
-if ($PCInfo.Manufacturer -like "*DELL*")
+function Program-Install
 {
-	$source = 'https://dl.dell.com/FOLDER06986472M/1/Dell-Command-Update-Application-for-Windows-10_DF2DT_WIN_4.1.0_A00.EXE'
-	$exeDir = 'C:\Windows\Temp'
-	$exeName = 'Dell-Command-Update-Application-for-Windows-10_DF2DT_WIN_4.1.0_A00.EXE'
-	$ArgumentList = '/s'
+	param (
+		[string]$installSource,
+		[string]$installName,
+		[string]$installArgument,
+		[string]$installTest,
+		[string]$workDir,
+		[string]$maxLoop,
+		[switch]$disableStartProcess = $true,
+		[switch]$deleteInstallSource = $true
+	)
 	
-	$exeLocation = $exeDir + '\' + $exeName
+	if ([string]::IsNullOrEmpty($maxLoop)) { $maxLoop = "5" } <# Loop max #>
+	$sleepTimer = "5" <# Sleep before next loop #>
 	
-	if ((Test-Path $exeLocation) -ne "True") { Invoke-WebRequest $source -OutFile $exeLocation | Wait-Job } <# Lookup if the exe is there #>
-	
-	Start-Process -FilePath $exeLocation -ArgumentList $ArgumentList | Wait-Job
-	
-	$32bit = "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"
-	$32bitLocation = Test-Path $32bit
-	$64bit = "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe"
-	$64bitLocation = Test-Path $64bit
-	
-	If ($32bitLocation -eq "True")
+	if ((Test-Path -Path $installTest) -ne "True")
 	{
-		Write-host "Updating Dell Drivers..."
-		Start-Process -FilePath $32bit -ArgumentList "/ApplyUpdates -reboot=disable"
+		if ((Test-Path -Path "$workDir\$installName") -ne "True") <# Lookup if the exe is there #>
+		{
+			if (Get-Command 'Invoke-Webrequest') { Invoke-WebRequest $installSource -OutFile "$workDir\$installName" | Wait-Job }
+			else
+			{
+				$WebClient = New-Object System.Net.WebClient
+				$webclient.DownloadFile($installSource, "$workDir\$installName")
+			}
+		}
+		
+		[int]$retryCount = "0" <# Loop starting at 0 #>
+		[int]$retryCountMax = $maxLoop <# Try max loops #>
+		$stopLoop = $false <# Base status for looping, stoping loop when True #>
+		
+		if ($disableStartProcess)
+		{
+			do <# Install and verify #>
+			{
+				if ((Test-Path -Path $installTest) -eq "True")
+				{
+					Write-Host "$installName installed!"
+					Start-Sleep -Seconds $sleepTimer
+					$stopLoop = $true
+				}
+				if ((Test-Path -Path $installTest) -ne "True")
+				{
+					if ([int]$retrycount -eq "0")
+					{
+						Write-Host "Running $installName install..."
+						Start-Process -FilePath "$workDir\$installName" -ArgumentList $installArgument -Wait <# Run the exe #>
+					}
+					$Retrycount = $Retrycount + 1
+					if ($Retrycount -gt $RetrycountMax)
+					{
+						Write-Host "$installName failed to install..."
+						$stopLoop = $true
+					}
+					else
+					{
+						Write-Host "Testing $installName Install, attempt number $retryCount, Waiting $sleepTimer secs..."
+						Start-Sleep -Seconds $sleepTimer
+					} <# Sleep before next loop #>
+				}
+			}
+			While ($stopLoop -eq $false)
+		}
 	}
-	If ($64bitLocation -eq "True")
+	if ($deleteInstallSource)
 	{
-		Write-host "Updating Dell Drivers..."
-		Start-Process -FilePath $64bit -ArgumentList "/ApplyUpdates -reboot=disable"
+		if ((Test-Path -Path "$workDir\$installName") -eq "True") { Remove-Item -Path "$workDir\$installName" -Force -Recurse }
 	}
-	
-	If (($32bitLocation -eq "False") -and ($64bitLocation -eq "False")) { Write-host 'Script failed to run "dcu-cli.exe" as the program was not found.' }
 }
 
-# Locate Lenovo PC Manufacturer
-if ($PCInfo.Manufacturer -like "*LENOVO*")
+function Program-Run
 {
-	Write-host "Updating Lenovo Drivers..."
-	Install-Module -Name LSUClient -Force
-    Import-Module -Name LSUClient -Force
-	$updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended }
-	$updates | Save-LSUpdate -Verbose
-	$updates | Install-LSUpdate -Verbose
+	param (
+		[string]$installName,
+		[string]$installArgument,
+		[string]$installTest,
+		[string]$workDir,
+		[switch]$deleteInstallSource = $true
+	)
+	
+	if ([string]::IsNullOrEmpty($maxLoop)) { $maxLoop = "5" } <# Loop max #>
+	$sleepTimer = "5" <# Sleep before next loop #>
+	
+	if ((Test-Path -Path $installTest) -ne "True")
+	{
+		[int]$retryCount = "0" <# Loop starting at 0 #>
+		[int]$retryCountMax = $maxLoop <# Try max loops #>
+		$stopLoop = $false <# Base status for looping, stoping loop when True #>
+		
+		do <# Install and verify #>
+		{
+			if ((Test-Path -Path $installTest) -eq "True")
+			{
+				Write-Host "$installName installed!"
+				Start-Sleep -Seconds $sleepTimer
+				$stopLoop = $true
+			}
+			if ((Test-Path -Path $installTest) -ne "True")
+			{
+				if ([int]$retrycount -eq "0")
+				{
+					Write-Host "Running $workDir\$installName install..."
+					if ([string]::IsNullOrEmpty($installArgument)) <# Run the exe #>
+					{ Start-Process -FilePath "$workDir\$installName" -Wait }
+					else
+					{ Start-Process -FilePath "$workDir\$installName" -ArgumentList $installArgument -Wait }
+				}
+				$Retrycount = $Retrycount + 1
+				if ($Retrycount -gt $RetrycountMax)
+				{
+					Write-Host "$workDir\$installName failed to install..."
+					$stopLoop = $true
+				}
+				else
+				{
+					Write-Host "Testing $workDir\$installName Install, attempt number $retryCount, Waiting $sleepTimer secs..."
+					Start-Sleep -Seconds $sleepTimer
+				} <# Sleep before next loop #>
+			}
+		}
+		While ($stopLoop -eq $false)
+	}
+	if ($deleteInstallSource)
+	{
+		if ((Test-Path -Path "$workDir\$installFolder") -eq "True") { Remove-Item -Path "$workDir\$installFolder" -Force -Recurse }
+	}
 }
 
+function Drivers-Install
+{
+	$PCInfo = Get-WMIObject -Query "Select * from Win32_ComputerSystem" | Select-Object -Property Manufacturer, Model
+	$PCmanufacturer = $PCInfo.Manufacturer
+	
+	if ($PCmanufacturer -like "*DELL*") <# Locate Dell PC Manufacturer #>
+	{
+		$installFolder = "Dell-Power-Manager"
+		Program-Install -installSource "https://dl.dell.com/FOLDER05695265M/3/Dell-Power-Manager-Service_H2VH9_WIN64_3.4.0_A00_01.EXE" -installName "$installFolder.exe" -installArgument "/s /e=$env:windir\Temp\$installFolder" -installTest "$env:windir\Temp\$installFolder\DPM_Setup64*" -workDir "$env:windir\Temp"
+		Program-Run -installName "$installFolder\DPM_Setup64_3_4_0.exe" -installArgument "/S /v/qn" -installTest "$env:ProgramFiles\Dell\CommandPowerManager\BatteryExtenderUtil.exe" -workDir "$env:windir\Temp"
+		
+		$installFolder = "Dell-Command-Update-Application"
+		Program-Install -installSource "https://dl.dell.com/FOLDER07414743M/2/Dell-Command-Update-Application_XM3K1_WIN_4.2.1_A00.EXE" -installName "$installFolder.exe" -installArgument "/s /e=$env:windir\Temp\$installFolder" -installTest "$env:windir\Temp\$installFolder\DCU_Setup*" -workDir "$env:windir\Temp"
+		Program-Run -installName "$installFolder\DCU_Setup_4_2_1.exe" -installArgument "/S /v/qn" -installTest "${env:ProgramFiles(x86)}\Dell\CommandUpdate\dcu-cli.exe" -workDir "$env:windir\Temp"
+		
+		try { Import-Module DellBIOSProvider -ErrorAction Stop }
+		catch
+		{
+			Write-Host "Could not find DellBIOSProvider, Installing..."
+			Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Verbose:$false *>$null
+			Install-Module -Name DellBIOSProvider -Scope AllUsers -force
+			Import-Module DellBIOSProvider -ErrorAction Stop
+		}
+		Set-Item -Path DellSmbios:\TpmSecurity\TpmSecurity "Enabled" -ErrorAction SilentlyContinue
+		Set-Item -Path DellSmbios:\TpmSecurity\TPMActivation "Enabled" -ErrorAction SilentlyContinue
+		
+		Write-host "Updating Dell Drivers..."
+		Start-Process -FilePath "${env:ProgramFiles(x86)}\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/ApplyUpdates -reboot=disable" -Wait
+	}
+	
+	if ($PCmanufacturer -like "*LENOVO*") <# Locate Lenovo PC Manufacturer #>
+	{
+		Write-host "Updating Lenovo Drivers..."
+		Program-Install -installSource "https://download.lenovo.com/consumer/options/lenovo_thinkpad_thunderbolt_3_dock_and_usb_c_dock_driver_v10019.exe" -installName "lenovo_dock_driver.exe" -installArgument "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART" -installTest "${env:ProgramFiles(x86)}\Lenovo\Thinkpad USB Ethernet Adapter Driver\RTINSTALLER64.EXE" -workDir "$env:windir\Temp" -StartProcess
+		
+		[ScriptBlock]$updateLenovoDrivers = {
+			try { Import-Module LSUClient -ErrorAction Stop }
+			catch
+			{
+				Write-Host "Could not find LSUClient, Installing..."
+				Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Verbose:$false *>$null
+				Install-Module -Name LSUClient -Scope AllUsers -force
+				Import-Module LSUClient -ErrorAction Stop
+			}
+			
+			#Get-AppxProvisionedPackage -Online | Where-Object { $_.name -like '*len*' }
+			#Get-AppxPackage | Where-Object { $_.PackageFullName -like '*len*' }
+			
+			$updateLenovo = Get-LSUpdate | Where-Object { $_.Installer.Unattended }
+			$updateLenovo | Save-LSUpdate -Verbose
+			$updateLenovo | Install-LSUpdate -Verbose
+		}
+		try { Invoke-AsCurrentUser -ScriptBlock $updateLenovoDrivers -CacheToDisk -ErrorAction Stop } <# Set Default app #>
+		catch
+		{
+			#Write-Host "You must run this script as system or have the SeDelegateSessionUserImpersonatePrivilege token." 
+			Invoke-Command -Command $updateLenovoDrivers
+		}
+		
+		# Get all BIOS settings (for reference)
+		# Get-WmiObject -class Lenovo_BiosSetting -namespace root\wmi | ForEach-Object {if ($_.CurrentSetting -ne "") {Write-Host $_.CurrentSetting.replace(","," = ")}}
+		
+		$Model = Get-WmiObject -Class Win32_ComputerSystemProduct
+		
+		if ($Model.version.TrimEnd() -like "*ThinkPad*") <# Laptop #>
+		{
+			(Get-WmiObject -class Lenovo_SetBiosSetting -namespace root\wmi).SetBiosSetting("SecurityChip,Active") <# Enable TPM #>
+			(Get-WmiObject -class Lenovo_SaveBiosSettings -namespace root\wmi).SaveBiosSettings() <# Save settings #>
+		}
+		
+		if ($Model.version.TrimEnd() -like "*ThinkCentre*") <# Desktop #>
+		{
+			(Get-WmiObject -class Lenovo_SetBiosSetting -namespace root\wmi).SetBiosSetting("TCG Security Feature,Active") <# Enable TPM #>
+			(Get-WmiObject -class Lenovo_SaveBiosSettings -namespace root\wmi).SaveBiosSettings() <# Save settings #>
+		}
+	}
+	
+	if (($PCmanufacturer -like "*HEWLETT*") -or ($PCmanufacturer -like "*HP*")) <# Locate HP PC Manufacturer #>
+	{
+		Write-host "Updating HP Drivers..."
+		
+		# Install HP-HPIA
+		$installFolder = "HP-HPIA"
+		$installTest = "$env:windir\Temp\$installFolder"
+		$installArgument = '/s /e /f ' + $installTest
+		Program-Install -installSource "https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.1.2.exe" -installName "$installFolder.exe" -installArgument $installArgument -installTest $installTest -workDir "$env:windir\Temp"
+		Start-Process -FilePath "$env:windir\Temp\$installFolder\ImageAssistant.exe" -ArgumentList '/Operation:Analyze /Category:All /Selection:All /Action:Install /SoftpaqDownloadFolder:"C:\Windows\Temp\HP-HPIA\Downloads" /Silent' -Wait
+		
+		# Install HP-SA
+		Program-Install -installSource "https://ftp.ext.hp.com/pub/softpaq/sp123001-123500/sp123485.exe" -installName "HP-SA.exe" -installArgument "/s" -installTest "${env:ProgramFiles(x86)}\HP\HP Support Framework\TaskbarController.exe" -workDir "$env:windir\Temp" -disableStartProcess:$false
+		[ScriptBlock]$scriptBlock = { Start-Process -FilePath "$env:windir\Temp\HP-SA.exe" -ArgumentList "/s" -Wait }
+		try { Invoke-AsCurrentUser -ScriptBlock $scriptBlock -CacheToDisk -ErrorAction Stop } <# Run program #>
+		catch { Invoke-Command -Command $scriptBlock }
+		
+		# Install HP-BCU
+		$installFolder = "HP-BCU"
+		$installTest = "$env:windir\Temp\$installFolder"
+		$installArgument = '/s /e /f ' + $installTest
+		Program-Install -installSource "https://ftp.ext.hp.com/pub/softpaq/sp107501-108000/sp107705.exe" -installName "$installFolder.exe" -installArgument $installArgument -installTest $installTest -workDir "$env:windir\Temp"
+		Program-Run -installName "$installFolder\Setup.exe" -installArgument "/S /v/qn" -installTest "${env:ProgramFiles(x86)}\HP\BIOS Configuration Utility\BiosConfigUtility.exe" -workDir "$env:windir\Temp"
+	}
+}
+
+function RunAsUser-Install
+{
+	try { Import-Module RunAsUser -ErrorAction Stop }
+	catch
+	{
+		Write-Host "Could not find RunAsUser, Installing..."
+		Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Verbose:$false *>$null
+		Install-Module -Name RunAsUser -Scope AllUsers -force
+		Import-Module RunAsUser -ErrorAction Stop
+	}
+}
+
+RunAsUser-Install
+Drivers-Install
